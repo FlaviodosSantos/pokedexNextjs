c5b7889bbb85dfe6e7472840e2913503c5df9497
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-10-28 13:03:06 -0300

    css header e footer - aula #20

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,8 +1,10 @@
+import styles from "../../styles/Footer.module.css";
+
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
-        <span>PokeNext</span> &copy 27.10.2022{" "}
+        <span>PokeNext</span> &copy; 27.10.2022{" "}
       </p>
     </footer>
   );
